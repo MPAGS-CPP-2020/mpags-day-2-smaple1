2b47b597e4e9d8e8cb1902c110239290675066c0
Stephen <stephen@stephen-vivobook>
2020-10-23 11:37:47 +0100

    Implemented function to process command line arguments

--- src/mpags-cipher.cpp
+++ src/mpags-cipher.cpp
@@ -7,6 +7,14 @@
 #include <cctype>
 // Forward declarations
 std::string transformChar(const char in_char);
+bool processCommandLine(const std::vector<std::string>& args,
+			bool& helpRequested,
+			bool& versionRequested,
+			std::string& inputFileName,
+			std::string& outputFileName );
+
+// Add a typedef that assigns another name for the given type for clarity
+typedef std::vector<std::string>::size_type size_type;
 
 
   // Initialise variables for processing input text
@@ -70,33 +78,25 @@ std::string transformChar(const char in_char)
     return newChar;
   }
 
-// Main function of the mpags-cipher program
-int main(int argc, char* argv[])
+bool processCommandLine(const std::vector<std::string>& args,
+			bool& helpRequested,
+			bool& versionRequested,
+			std::string& inputFile,
+			std::string& outputFile)
 {
-  // Convert the command-line arguments into a more easily usable form
-  const std::vector<std::string> cmdLineArgs {argv, argv+argc};
-
-  // Add a typedef that assigns another name for the given type for clarity
-  typedef std::vector<std::string>::size_type size_type;
-  const size_type nCmdLineArgs {cmdLineArgs.size()};
-
-  // Options that might be set by the command-line arguments
-  bool helpRequested {false};
-  bool versionRequested {false};
-  std::string inputFile {""};
-  std::string outputFile {""};
+  const size_type nCmdLineArgs {args.size()};
 
   // Process command line arguments - ignore zeroth element, as we know this to
   // be the program name and don't need to worry about it
   for (size_type i {1}; i < nCmdLineArgs; ++i) {
 
-    if (cmdLineArgs[i] == "-h" || cmdLineArgs[i] == "--help") {
+    if (args[i] == "-h" || args[i] == "--help") {
       helpRequested = true;
     }
-    else if (cmdLineArgs[i] == "--version") {
+    else if (args[i] == "--version") {
       versionRequested = true;
     }
-    else if (cmdLineArgs[i] == "-i") {
+    else if (args[i] == "-i") {
       // Handle input file option
       // Next element is filename unless -i is the last argument
       if (i == nCmdLineArgs-1) {
@@ -106,11 +106,11 @@ int main(int argc, char* argv[])
       }
       else {
 	// Got filename, so assign value and advance past it
-	inputFile = cmdLineArgs[i+1];
+	inputFile = args[i+1];
 	++i;
       }
     }
-    else if (cmdLineArgs[i] == "-o") {
+    else if (args[i] == "-o") {
       // Handle output file option
       // Next element is filename unless -o is the last argument
       if (i == nCmdLineArgs-1) {
@@ -120,18 +120,17 @@ int main(int argc, char* argv[])
       }
       else {
 	// Got filename, so assign value and advance past it
-	outputFile = cmdLineArgs[i+1];
+	outputFile = args[i+1];
 	++i;
       }
     }
     else {
       // Have an unknown flag to output error message and return non-zero
       // exit status to indicate failure
-      std::cerr << "[error] unknown argument '" << cmdLineArgs[i] << "'\n";
+      std::cerr << "[error] unknown argument '" << args[i] << "'\n";
       return 1;
     }
   }
-
   // Handle help, if requested
   if (helpRequested) {
     // Line splitting for readability
@@ -157,8 +156,6 @@ int main(int argc, char* argv[])
     std::cout << "0.1.0" << std::endl;
     return 0;
   }
-
-
   // Output the transliterated text
   // Warn that output file option not yet implemented
   if (!outputFile.empty()) {
@@ -166,13 +163,42 @@ int main(int argc, char* argv[])
               << outputFile
               << "') not implemented yet, using stdout\n";
   }
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Main function of the mpags-cipher program
+int main(int argc, char* argv[])
+{
+  // Convert the command-line arguments into a more easily usable form
+  const std::vector<std::string> args {argv, argv+argc};
+  bool helpRequested {false};
+  bool versionRequested {false};
+  std::string inputFile {""};
+  std::string outputFile {""};
+
+  processCommandLine(args,
+		     helpRequested,
+		     versionRequested,
+		     inputFile,
+		     outputFile);
 
   while(std::cin >> inputChar)
     {
-      std::cout << transformChar(inputChar);
+      inputText+=transformChar(inputChar);
     }
   std::cout << inputText << std::endl;
-
   // No requirement to return from main, but we do so for clarity
   // and for consistency with other functions
   return 0;
